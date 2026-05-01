468ca99d1a7df5f943ca7f9deca579938b4438d0
Sohel Shaikh <sohelshaikh97@gmail.com>
2026-05-01 23:53:55 +0530

    fix: ensure map takes full screen using ignoresSafeArea

--- BeenThere/UI/ContentView.swift
+++ BeenThere/UI/ContentView.swift
@@ -6,15 +6,16 @@ struct ContentView: View {
     var body: some View {
         ZStack(alignment: .top) {
             IndiaMapView(visitManager: visitManager)
-                .edgesIgnoringSafeArea(.all)
+                .ignoresSafeArea()
             
             Text("Been There")
                 .font(.system(size: 24, weight: .bold, design: .rounded))
                 .padding()
                 .background(Color.white.opacity(0.7))
                 .cornerRadius(10)
-                .padding(.top, 50)
+                .padding(.top, 10) // Reduced top padding
         }
+        .ignoresSafeArea()
     }
 }
 
